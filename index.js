3060733e2b7ade8a796c5df94f4930b6bd26b12e
ahmed abd elwahab <ahmed.almelegy617@el-eng.menofia.edu.eg>
2019-11-30 11:42:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array){
+  array.forEach((element, index) =>{
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback){
+  arr.forEach(callback);
+}
+
+function doToArray(ar, callback){
+  ar.forEach(callback);
+}
